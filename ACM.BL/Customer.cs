d3b7d629db1365959d028b52e9e313610592c631
Cal-Xavi <callummander@gmail.com>
2022-09-23 13:47:02 +0100

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string FullName
+        {
+            get { return firstName + "," + lastName; }
+        }
+    }
+}
